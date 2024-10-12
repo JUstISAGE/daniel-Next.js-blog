6c3fbbf7cf6310becf0cfddfdbb03881d65fade5
Shuhan Lyu <shuhanlyu@ucsb.edu>
2024-10-12 01:29:56 -0700

    fix suspense issue in page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,8 +1,17 @@
-'use client'
+'use client';
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
-export default function Page() {
+function PageContent() {
     const searchParams = useSearchParams();
     const username = searchParams.get('username');
-    return <h1 className={"text-4xl text-black"}>Hello, {username}!</h1>
+    return <h1 className="text-4xl text-black">Hello, {username}!</h1>;
+}
+
+export default function Page() {
+    return (
+        <Suspense fallback={<h1>Loading...</h1>}>
+            <PageContent />
+        </Suspense>
+    );
 }
